9ef2a62830af4af47620b81974d2cda577b74bff
Bittech <bittech@bittechs-imac-4.lan>
2019-10-08 19:41:36 -0400

    modified

--- bddprojectbtc190613e/src/test/java/com/RUNNER.java
+++ bddprojectbtc190613e/src/test/java/com/RUNNER.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features= {"/Users/bittech/eclipse-workspace/bddprojectbtc190613e/login.feature"},tags= {"@Smoke"},
-plugin ={"pretty" , "html:login"},glue= {"com"})
+plugin ={"pretty" , "html:login"},glue= {"com"},dryRun=true)
 public class RUNNER {
 
 	
